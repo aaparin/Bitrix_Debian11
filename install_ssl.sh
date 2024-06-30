97cfb2ddd84c4e118da0e2c841dc9106ad6f0e8d
Anton Aparin <anton.aparin@dreamsite.lt>
2024-06-30 14:25:27 +0300

    ашч ыfix ssh

--- install_ssl.sh
+++ install_ssl.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
 
 # Check if the script is running as root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
 # Request website domain
 read -p "Enter your website domain (if not, a certificate will not be obtained): " domain
 
-# ChecK letsencrypt INSTALL
+# Check if Certbot is installed
 if [ -f /usr/bin/letsencrypt ]; then
     echo "Letsencrypt is installed"
 else
     echo "Letsencrypt is not installed"
     echo "Installing Letsencrypt..."
+    apt update
     apt install certbot python3-certbot-nginx -y
 fi
 
-# Check if the domain is set
-# Проверка и обновление конфигурации Nginx
+# Paths to configuration files
+template_conf="config/nginx_config.conf"
 nginx_conf="/etc/nginx/sites-available/$domain"
+
+# Check if the Nginx configuration for the domain exists
 if [ ! -f "$nginx_conf" ]; then
     echo "Creating Nginx configuration for $domain..."
-    cat > "$nginx_conf" <<EOL
-server {
-    listen 80;
-    server_name $domain www.$domain;
-
-    location / {
-        proxy_pass http://localhost:8080; # измените это на нужный вам backend
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-EOL
-
+    # Copy the template configuration file to the Nginx sites-available directory
+    cp "$template_conf" "$nginx_conf"
+    # Replace placeholders with the actual domain name
+    sed -i "s/{{user_domain}}/$domain/g" "$nginx_conf"
+    # Create a symbolic link in the sites-enabled directory
     ln -s "$nginx_conf" /etc/nginx/sites-enabled/
+    # Test Nginx configuration and reload if successful
     nginx -t && systemctl reload nginx
 fi
 
 echo 'Creating SSL certificate...'
-certbot --nginx -d $domain
-
-
+# Obtain an SSL certificate for the domain and www.domain
+certbot --nginx -d $domain -d www.$domain
